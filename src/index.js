e3bf8c659cdab93842c81dbbe6bd02483ad22612
Bruno Allevato <brunoallevato@gmail.com>
2023-06-08 14:08:42 +0100

    new react project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+// 1) Import the React and ReactDom libraries
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) Get a reference to the div with ID root
+const el = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3) Tell React to take control of that element
+const root = ReactDom.createRoot(el);
+
+// 4) Create a component
+function App() {
+  const inputType ='number';
+  const minValue = 5;
+  const maxValue =10;
+  const colorr = { color: 'red' };
+  const listOpt = [1,2,3];
+  const message = 'Enter age';
+
+  return <input type={inputType} min={minValue} max={ maxValue } style={ colorr } list={[listOpt]} alt={message} />;
+}
+
+// 5) Show the component on the screen
+root.render(<App />);
